5484fd7277ab45825544b9ae32c6245f3089698c
Rasmus Buchholdt <RasmusBuchholdt@users.noreply.github.com>
2019-10-16 15:12:58 +0200

    1.0.4

--- src/index.ts
+++ src/index.ts
@@ -3,68 +3,68 @@ import { parseXML } from './util';
 let request = require('request-promise');
 
 export class PlexWrapper {
+  private clientId: string = '';
+  private username: string = '';
+  private password: string = '';
+  private accessToken: string = '';
 
-    private clientId: string = "";
-    private username: string = "";
-    private password: string = "";
-    private accessToken: string = "";
+  constructor(clientId: string, username: string, password: string) {
+    this.clientId = clientId;
+    this.username = username;
+    this.password = password;
+  }
 
-    constructor(clientId: string, username: string, password: string) {
-        this.clientId = clientId;
-        this.username = username;
-        this.password = password;
-    }
+  authenticate(): Promise<any> {
+    let options: {} = {
+      method: 'POST',
+      url: 'https://plex.tv/api/v2/users/signin',
+      json: true,
+      headers: {
+        'X-Plex-Client-Identifier': this.clientId,
+        'X-Plex-Device-Name': '',
+        'X-Plex-Product': '',
+        'X-Plex-Device': '',
+        'X-Plex-Version': '',
+      },
+      form: {
+        login: this.username,
+        password: this.password,
+      },
+    };
 
-    authenticate(): Promise<any> {
-        let options: {} = {
-            method: "POST",
-            url: "https://plex.tv/api/v2/users/signin",
-            json: true,
-            headers: {
-                "X-Plex-Client-Identifier": this.clientId,
-                "X-Plex-Device-Name": "",
-                "X-Plex-Product": "",
-                "X-Plex-Device": "",
-                "X-Plex-Version": ""
-            },
-            form: {
-                login: this.username,
-                password: this.password
-            },
-        };
-
-        return new Promise<any>((resolve: any) => {
-            request(options)
-                .then((result: { authToken: string; }) => {
-                    this.accessToken = result.authToken;
-                    resolve();
-                })
-                .catch((error: any) => {
-                    throw new Error(error.message)
-                });
+    return new Promise<any>((resolve: any) => {
+      request(options)
+        .then((result: { authToken: string }) => {
+          this.accessToken = result.authToken;
+          resolve();
+        })
+        .catch((error: any) => {
+          throw new Error(error.message);
         });
-    }
+    });
+  }
 
-    getServers(): Promise<any> {
-        if (this.accessToken === "") return this.authenticate().then(() => this.getServers());
-        let options: {} = {
-            method: "GET",
-            url: `https://plex.tv/api/servers`,
-            json: true,
-            headers: {
-                "X-Plex-Token": this.accessToken,
-            },
-        };
+  getServers(): Promise<any> {
+    if (this.accessToken === '') return this.authenticate().then(() => this.getServers());
+    let options: {} = {
+      method: 'GET',
+      url: `https://plex.tv/api/servers`,
+      json: true,
+      headers: {
+        'X-Plex-Token': this.accessToken,
+      },
+    };
 
-        return new Promise((resolve: any) => {
-            request(options)
-                .then((result: any) => {
-                    parseXML(result).then((parsedResult: any)  => {
-                        resolve(parsedResult.MediaContainer.Server);
-                    });
-                }).catch((error: any) => {
-                    throw new Error(error.message)
-                });
+    return new Promise((resolve: any) => {
+      request(options)
+        .then((result: any) => {
+          parseXML(result).then((parsedResult: any) => {
+            resolve(parsedResult.MediaContainer.Server);
+          });
+        })
+        .catch((error: any) => {
+          throw new Error(error.message);
         });
-    }
+    });
+  }
 }
--- src/util.ts
+++ src/util.ts
@@ -1,10 +1,10 @@
 let xml2js = require('xml2js');
 
 export function parseXML(xmlString: string): Promise<string> {
-    let parser = new xml2js.Parser();
-    return new Promise((resolve: any) => {
-        parser.parseStringPromise(xmlString).then((result: any) => {
-            resolve(result);
-        });
+  let parser = new xml2js.Parser();
+  return new Promise((resolve: any) => {
+    parser.parseStringPromise(xmlString).then((result: any) => {
+      resolve(result);
     });
+  });
 }
